41cc964b745c7bd0c99f108126355d37be410b84
unknown <mfnour08@gmail.com>
2022-12-30 17:06:09 +0700

    commit

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,21 +2,15 @@ $(document).ready(function(){
   $(".spec-images-rumah").css("border-radius", "1rem 1rem");
   $(".real-images").css("border-radius", "3rem 3rem");
 
-  function contactLink(whatsapp){
-    window.open("https://wa.me/6281291726369?text=Halo,%20boleh%20minta%20pricelist%20Mesa%20Grande")
-  }
-  function contactLink(instagram){
-    window.open("https://www.instagram.com/hip_property/")
-  }
-  function contactLink(facebook){
-    window.open("https://www.facebook.com/hipproperty/")
-  }
-  function contactLink(email){
-    window.open("https://wa.me/6281291726369?text=Halo,%20boleh%20minta%20pricelist%20Mesa%20Grande")
-  }
-
   $(".contact-link").click(function(){
     var link = $(this).attr("id");
-    contactLink(link);
-  })
+    console.log(link);
+    if (link === "whatsapp") {
+      window.open("https://wa.me/6281291726369?text=Halo,%20boleh%20minta%20pricelist%20Mesa%20Grande");
+    }else if (link === "instagram") {
+      window.open("https://www.instagram.com/hip_property/")
+    }else if (link === "facebook") {
+      window.open("https://www.facebook.com/hipproperty/")
+    }
+  });
 });
